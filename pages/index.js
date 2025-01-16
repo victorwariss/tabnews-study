075b9ee9900094015472fc68638648150e4d3aa5
Victor Wariss <victor.wariss@outlook.com>
2025-01-16 07:45:49 +0000

    Commit incial - Desafio do impacto

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+    return (
+    <div>
+        <h1>Meu amor,</h1>
+        <p>Sei que tenho muito que melhorar , mas estou correndo atrás das coisas, vamos vencer!</p>
+    </div>
+    )
+}
+
+export default Home;
